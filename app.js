14e6f617b4243281f846660a79dd9d1ba87a96ec
Erick Fernando Carvalho Sanchez <carvalhosanchezerickfernando@gmail.com>
2025-02-21 09:35:20 -0300

    Part 11 Health Check Fix

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.use(express.static('dist'))
 
 // eslint-disable-next-line no-unused-vars
 app.get('/health', (req, res) => {
-  throw('error...')
-  // res.send('ok')
+  //throw('error...')
+  res.send('ok')
 })
 
 app.listen(PORT, () => {

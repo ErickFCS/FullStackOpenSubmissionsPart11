a9ddf7e880062d43c26f360a8da8b2c58ef92ba5
Erick Fernando Carvalho Sanchez <carvalhosanchezerickfernando@gmail.com>
2025-02-21 09:30:21 -0300

    Part 11 Health Check Error

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+// eslint-disable-next-line no-unused-vars
 app.get('/health', (req, res) => {
-  res.send('ok')
+  throw('error...')
+  // res.send('ok')
 })
 
 app.listen(PORT, () => {
